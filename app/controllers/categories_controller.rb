f1f46a9b9f58e6bfee641ffa6c91de6b23ef2edb
hamze <hamzeahmedx@gmail.com>
2017-09-05 17:48:47 +0000

    Only Admin can Make Category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController 
-
+before_action  :require_admin , except: [:show , :index] 
   
   def index
     @categories = Category.all
@@ -34,6 +34,13 @@ class CategoriesController < ApplicationController
       if current_user != @page.user and !current_user.admin?
         flash[:danger] = "Cidii Leh Uunbaa Wax Kabdali Karta"
         redirect_to root_path
+      end
+    end
+    
+    def require_admin
+      if !logged_in? || (logged_in? and !current_user.admin?)
+        flash[:danger] = "Only Admin Cam Preform This Action"
+        redirect_to categories_path
       end
     end
 end
--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require  'test_helper'
 class CategoriesControllerTest < ActionController::TestCase
   def setup
     @category = Category.create(name: "sport")
+    @user = User.create(username: "John" , email: "john@example.com" , password: "123" , admin: true)
   end
   test "should get categories index" do
     get :index
@@ -10,6 +11,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get categories new" do
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -21,8 +23,16 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test "should get categories edit" do
+    session[:user_id] = @user.id
     get(:edit , {'id' => @category.id}) 
     assert_response :success
+  end
+  
+  test "should redirect create when admin not logged_in " do
+    assert_no_difference 'Category.count' do
+      post :create , category: {name: "sports"}
+    end
+    assert_redirected_to categories_path
   end
   
 end
